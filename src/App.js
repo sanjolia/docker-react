fcdd35ab4ffe8138ea281908d1d3217ad84c5ebf
Sanjoli Agarwal <sanjolia@amazon.com>
2024-02-01 22:30:44 -0800

    changed app text

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Ciao Bella!
+          Ciao Bella! New Feature!
         </p>
         <a
           className="App-link"
